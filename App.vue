82e28902d495f938921da98c9c983f890b65be78
rahulraj-git <rhul.m.rj@gmail.com>
2024-10-07 17:11:34 +0530

    basic setup done

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <NuxtPage />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+/* Your styles here */
+</style>
